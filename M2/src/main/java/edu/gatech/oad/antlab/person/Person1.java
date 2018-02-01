80b1c13f6e3cb3ffd9fce436f3993516239ea571
obecerra3 <obecerra3@gatech.edu>
2018-01-31 21:41:40 -0500

    fixed simple issues in person files

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-		if (input.length > 2) {
+		if (input.length() > 2) {
 			return (input.substring(2) + input.substring(0, 2));
 		} else {
 			return input;
