6178474a6610623d2d58420d20ebe9fed442cc2f
bisi766 <adebisithomp@gatech.edu>
2018-01-31 23:09:20 -0500

    Implemented calc method in Person4.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String emptyString = "";
+      for (int i = 0; i < input.length(); i++) {
+        emptyString += (char) (input.charAt(i) + 1);
+      }
+      return emptyString;
     }
     
     /**
